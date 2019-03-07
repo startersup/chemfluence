4ef5c47346b205a19fa5d59858478529287cb48d
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-03-07 20:42:29 +0530

    Revert "login"
    This reverts commit e664fbec34123ef08491bf337e98b23aab51125e.

--- logout/index.php
+++
@@ -1,8 +0,0 @@
-<<?php
-session_start();
-session_unset($_SESSION['id']);
-session_unset($_SESSION['mode']);
-session_unset($_SESSION['orderid']);
-session_destroy();
-header('location: /index.php');
- ?>
