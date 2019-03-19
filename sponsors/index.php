b3a9d70233b24c74f8418c66fb92936494d64abf
saicharan96 <saicharan14996@gmail.com>
2019-03-19 19:47:32 +0530

    Update index.php
    ddd

--- sponsors/index.php
+++ sponsors/index.php
@@ -154,7 +154,6 @@ $_SESSION['redirect']='/sponsors';
                             <div class="col-md-4 col-xs-6">
                                 <div class="cardin eventspace" id="borderimg2" style="background-color:#000000;">
                                    <img class="imager" src="../assets/images/slogo1.png"><br>
-                                   
                                 </div>
                                 
                     
@@ -174,7 +173,31 @@ $_SESSION['redirect']='/sponsors';
                     
                 </div>
                               
-                          </div></center></div></div></div>
+                          </div>
+
+
+<div class="row">
+                            <div class="col-md-4 col-xs-6">
+                                <div class="cardin eventspace" id="borderimg2" style="background-color:#000000;">
+                                   <img class="imager" src="../assets/images/slogo4.png"><br>
+                                </div>
+                                
+                    
+                </div>
+                       <div class="col-md-4 col-xs-6">
+                                <div class="cardin eventspace" id="borderimg2">
+                                   <img class="imager" src="../assets/images/slogo5.jpg"><br>
+                                 
+                                </div>
+                </div>
+                             
+                               
+                    
+                </div>
+                              
+                          </div>
+
+                      </center></div></div></div>
 
 
 
