4a9040e3f7a062254de79a5823135452edc3f766
saicharan96 <saicharan14996@gmail.com>
2019-03-22 22:58:05 +0530

    ss
    sss

--- sponsors/index.php
+++ sponsors/index.php
@@ -172,7 +172,15 @@ $_SESSION['redirect']='/sponsors';
                     
                 </div>
           
-                              
+                     <div class="col-md-4 col-xs-6">
+                                <div class="cardin eventspace" id="borderimg2">
+                                   <img class="imager" src="../assets/images/sosa.jpg"><br>
+                                    
+                                </div>
+                               
+                    
+                </div>
+                    
                           </div>
 
 
