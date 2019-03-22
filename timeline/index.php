3c11608a1ab691e9805eb92b1aa569a236b969ac
saicharan96 <saicharan14996@gmail.com>
2019-03-22 23:44:47 +0530

    Update index.php
    sss

--- timeline/index.php
+++ timeline/index.php
@@ -355,15 +355,15 @@ $_SESSION['redirect']='/timeline';
             </div>
           </div>
         </li><br>
-        <li>
-          <div class="timeline-badge warning"><img class="imge1" src="../assets/images/craftsman.png"></div>
+       <li class="timeline-inverted">
+          <div class="timeline-badge info"><img  class="imge1" src="../assets/images/craftsman.png"></div>
           <div class="timeline-panel">
             <div class="timeline-heading">
-              <h4 class="timeline-title">WORKSHOP-3 </h4>
-              <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 9:00 AM - 1:00 PM</small></p>
+              <h4 class="timeline-title">Lunch</h4>
+              <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 1 PM – 2 PM</small></p>
             </div>
             <div class="timeline-body">
-              <p></p>
+              <p>The Partcipants can have their lunch and get back to their events.</p>
             </div>
           </div>
         </li><br>
