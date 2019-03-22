b13718769b95cb92330be95dd912f31f5c8ca209
saicharan96 <saicharan14996@gmail.com>
2019-03-22 22:40:08 +0530

    Update index.php
    ddd

--- sponsors/index.php
+++ sponsors/index.php
@@ -189,6 +189,12 @@ $_SESSION['redirect']='/sponsors';
                                  
                                 </div>
                 </div>
+                       <div class="col-md-4 col-xs-6">
+                                <div class="cardin eventspace" id="borderimg2">
+                                   <img class="imager" src="../assets/images/poorvika.jpg"><br>
+                                 
+                                </div>
+                </div>
                              
                                
                     
